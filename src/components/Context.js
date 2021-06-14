427ef8f25c112120bcc55f28cfae4f228ac660d7
DodoBey <dogukanyigiter@gmail.com>
2021-06-14 02:34:02 -0700

    sunday update

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,51 +1,148 @@
+import RainyVideo from '../videos/rainy.mp4';
+import CloudyVideo from '../videos/cloudy.mp4';
+import SnowyVideo from '../videos/snowy.mp4';
+import ClearVideo from '../videos/clear.mp4';
 import React from 'react';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SEARCH_CITY":
+            return {
+                city: action.payload
+            };
+        default:
+            return state;
+    }
+}
+
 export class Provider extends React.Component {
     state = {
         allData: [],
-        city: "Vancouver",
+        city: "Istanbul",
+        dispatch: (action) => {
+            this.setState((state) => reducer(state, action))
+        },
+        status: "",
+        mainTemp: "",
+        feelsLike: "",
+        windSpeed: "",
+        humidity: "",
+        sunRise: "",
+        sunSet: "",
+        lat: "",
+        lon: "",
+        bgVideo: "",
         units: "metric",
+        weekly: [],
         key: "db27287803721756d273ad76e35706ea",
         mainUrl: "https://api.openweathermap.org/data/2.5/",
     }
-    // fetchData(city){
-    //     fetch(`${this.state.mainUrl}weather?q=${this.state.city}&units=metric&appid=${this.state.key}`).then(response =>{
-    //         if (response.status !== 200){
-    //             console.log(`Houston we have a problem! It's = ${response.status}`)
-    //         }
-    //         response.json().then(data => {
-    //             this.setState({
-    //                 allData: data
-    //             });
-    //         })
-    //         .catch((error) => {
-    //             console.log(`Houston, we still have a problem! It's = ${error}`)
-    //         })
-    //     })
-    // }
 
-
-    componentDidMount() {
-        // this.fetchData("Vancouver");
-        // console.log(this.state.allData)
-        fetch(`${this.state.mainUrl}weather?q=${this.state.city}&units=${this.state.units}&appid=${this.state.key}`).then(response =>{
-            if (response.status !== 200){
+    localTime(time) {
+        const localeValue = new Date(time * 1000);
+        const localeTime = localeValue.toLocaleTimeString([], {timeStyle: 'short'});
+        return (
+            localeTime
+        )
+    }
+    fetchData() {
+        fetch(`${this.state.mainUrl}weather?q=${this.state.city}&units=${this.state.units}&appid=${this.state.key}`).then(response => {
+            if (response.status !== 200) {
                 console.log(`Houston we have a problem! It's = ${response.status}`)
             }
             response.json().then(data => {
                 this.setState({
-                    allData: data
+                    allData: data,
+                    status: data.weather[0].main,
+                    mainTemp: data.main.temp,
+                    feelsLike: data.main.feels_like,
+                    windSpeed: data.wind.speed,
+                    humidity: data.main.humidity,
+                    sunRise: this.localTime(data.sys.sunrise + data.timezone),
+                    sunSet: this.localTime(data.sys.sunset + data.timezone),
+                    lat: data.coord.lat,
+                    lon: data.coord.lon
                 });
             })
-            .catch((error) => {
-                console.log(`Houston, we still have a problem! It's = ${error}`)
+                .catch((error) => {
+                    console.log(`Houston, we still have a problem! It's = ${error}`)
+                })
+        })
+    }
+
+    fetchWeekly() {
+        fetch(`${this.state.mainUrl}onecall?lat=${this.state.lat}&lon=${this.state.lon}&units=${this.state.units}&appid=${this.state.key}`).then(response => {
+            if (response.status !== 200) {
+                console.log(`error ${response.status}`)
+            }
+            response.json().then(weeklyData => {
+                this.setState({
+                    weekly: weeklyData.daily
+                })
+            }).catch(error => {
+                console.log(`error ${error}`)
             })
         })
     }
 
-    render(){
+    bgvideoChanger() {
+        let value = this.state.status;
+        switch (value) {
+            case "Clouds":
+                this.setState({
+                    bgVideo: <source src={CloudyVideo} type='video/mp4' />
+                })
+                break;
+            case "Rain":
+                this.setState({
+                    bgVideo: <source src={RainyVideo} type='video/mp4' />
+                })
+                break;
+            case "Snow":
+                this.setState({
+                    bgVideo: <source src={SnowyVideo} type='video/mp4' />
+                })
+                break;
+            case "Clear":
+                this.setState({
+                    bgVideo: <source src={ClearVideo} type='video/mp4' />
+                })
+                break;
+            case "":
+                this.setState({
+                    bgVideo: <source src={ClearVideo} type='video/mp4' />
+                })
+                break;
+        }
+    }
+
+
+    componentDidMount() {
+        this.fetchData();
+        this.bgvideoChanger()
+        setTimeout(() => {
+            this.fetchWeekly()
+            this.bgvideoChanger()
+        }, 1000);
+    };
+
+    componentDidUpdate(prevState) {
+        if (this.city !== prevState.city) {
+            this.fetchData();
+            setTimeout(() => {
+                this.fetchWeekly()
+            }, 1000);
+        }
+        console.log("updated")
+    }
+
+
+
+
+
+    render() {
         return (
             <Context.Provider value={this.state}>
                 {this.props.children}
